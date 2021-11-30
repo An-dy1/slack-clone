f2d4859caedab3395029df69d113063fdaf28ba3
Andy <andrea_sue2@icloud.com>
2021-11-30 12:59:06 -0800

    stuck on update profile method - user display name and photo url are null and console.log statements don't display user info

--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { initializeApp } from 'firebase/app';
 import 'firebase/auth';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import md5 from 'md5';
 import {
   Grid,
   Form,
@@ -31,28 +31,116 @@ class Register extends React.Component {
     email: '',
     password: '',
     passwordConfirmation: '',
+    errors: [],
+    loading: false,
   };
+
+  isFormValid = () => {
+    let errors = [];
+    let error;
+
+    if (this.isFormEmpty(this.state)) {
+      error = { message: 'Please fill in all fields' };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else if (!this.isPasswordValid(this.state)) {
+      error = { message: `Passwords don't match or are invalid` };
+      this.setState({ errors: errors.concat(error) });
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  isFormEmpty = ({ username, email, password, passwordConfirmation }) => {
+    // if a field is empty, then its length will be 0 and falsy
+    // return the opposite of this, which would return true if any field's length is 0
+    return (
+      !username.length ||
+      !email.length ||
+      !password.length ||
+      !passwordConfirmation.length
+    );
+  };
+
+  isPasswordValid = ({ password, passwordConfirmation }) => {
+    if (password.length < 6 || passwordConfirmation.length < 6) {
+      return false;
+    } else if (password !== passwordConfirmation) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  displayErrors = (errors) =>
+    errors.map((error, i) => <p key={i}>{error.message}</p>);
 
   handleUserFormEntry = (event) => {
     this.setState({ [event.target.name]: event.target.value });
   };
 
+  updateCurrentUserProfile = (currentUser) => {
+    firebase.auth().onAuthStateChanged(function (currentUser) {
+      currentUser.user.updateProfile({
+        displayName: 'blue',
+        photoURL: `http://gravatar.com/avatar/${md5(
+          currentUser.user.email
+        )}?d=identicon`,
+      });
+    });
+  };
+
   handleSubmit = (event) => {
     // prevent the default submit action, which is to reload the page
     event.preventDefault();
-    firebase
-      .auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then((createdUser) => {
-        console.log(createdUser);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
+    if (this.isFormValid()) {
+      this.setState({ errors: [], loading: true });
+      firebase
+        .auth()
+        .createUserWithEmailAndPassword(this.state.email, this.state.password)
+        .then((createdUser) => {
+          // this code doesn't work
+          // might be solveable: https://stackoverflow.com/questions/59546023/firebase-javascript-auth-user-displaying-null-even-though-user-is-signed-in
+          createdUser.user
+            .updateProfile({
+              displayName: 'blue',
+              photoURL: `http://gravatar.com/avatar/${md5(
+                createdUser.user.email
+              )}?d=identicon`,
+            })
+            .then(() => {
+              this.setState({ loading: false });
+            })
+            .catch((err) => {
+              console.error(err);
+              this.setState({
+                error: this.state.errors.concat(err),
+                loading: false,
+              });
+            });
+        })
+        .catch((err) => {
+          console.error(err);
+          this.setState({
+            errors: this.state.errors.concat(err),
+            loading: false,
+          });
+        });
+    }
+  };
+
+  handleInputError = (errors, inputName) => {
+    return errors.some((error) =>
+      error.message.toLowerCase().includes(inputName)
+    )
+      ? 'error'
+      : '';
   };
 
   render() {
-    const { username, email, password, passwordConfirmation } = this.state;
+    const { username, email, password, passwordConfirmation, errors, loading } =
+      this.state;
 
     return (
       <Grid textAlign='center' verticalAlign='middle' className='app'>
@@ -82,6 +170,7 @@ class Register extends React.Component {
                 onChange={this.handleUserFormEntry}
                 value={email}
                 type='text'
+                className={this.handleInputError(errors, 'email')}
               ></Form.Input>
               <Form.Input
                 fluid
@@ -92,6 +181,7 @@ class Register extends React.Component {
                 onChange={this.handleUserFormEntry}
                 value={password}
                 type='password'
+                className={this.handleInputError(errors, 'password')}
               ></Form.Input>
               <Form.Input
                 fluid
@@ -102,13 +192,28 @@ class Register extends React.Component {
                 onChange={this.handleUserFormEntry}
                 value={passwordConfirmation}
                 type='password'
+                className={this.handleInputError(errors, 'password')}
               ></Form.Input>
 
-              <Button color='orange' fluid size='large'>
+              <Button
+                disabled={loading}
+                className={loading ? 'loading' : ''}
+                color='orange'
+                fluid
+                size='large'
+              >
                 Submit
               </Button>
             </Segment>
           </Form>
+
+          {errors.length > 0 && (
+            <Message error>
+              <h3>Error</h3>
+              {this.displayErrors(errors)}
+            </Message>
+          )}
+
           <Message>
             Already a user?<Link to='/login'> Login</Link>
           </Message>
